10f7527b86b05a948495706e35c78287dba69781
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2023-01-22 17:47:49 -0500

    ejercicio hospital

--- hola.js
+++ hola.js
@@ -11,20 +11,27 @@ const formarGrupos = (N = 0, K = 0, T = []) => {
             } else if (j > i) {
                 resultados_maximos[i][j] = resultados_maximos[i][j - 1]
             } else {
-                resultados_maximos[i][j] = Math.max(resultados_maximos[i - 1][j] + T[i - 1], resultados_maximos[i - j][j - 1] + T[i - 1])
+                console.log("Math.max(T.slice(0, 3)): ", Math.max(...T.slice(i - j - 1, i)))
+                console.log("T.slice(i - j - 1, i).length: ", T.slice(i - j - 1, i).length)
+                resultados_maximos[i][j] = Math.max(resultados_maximos[i - 1][j] + T[i - 1], Math.max(...T.slice(i - j - 1, i)) * T.slice(i - j - 1, i).length)
+
             }
         }
     }
     console.log("resultados_maximos: ", resultados_maximos)
 }
 
+/* const N1 = 10000;
+let T1 = []
+for (let i = 0; i < N1; i++) {
+    T1.push(1)
+} */
 
-
-// const T = [1, 15, 7, 9, 2, 5, 10]; //84
+const T = [1, 15, 7, 9, 2, 5, 10]; //84
 // const T = [1, 15, 7, 10, 2, 5, 10]; //85
 // const T = [10, 20, 7, 1, 2, 5, 1]; //85
 // const T = [10, 20, 7, 1]; //70
-const T = [10, 20, 7, 1, 2, 5]; //80
+// const T = [10, 20, 7, 1, 2, 5]; //80
 const K = 3
 const N = T.length
 
