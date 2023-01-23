92236a1bb257b1dad04058d06230d66e72b688cd
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-01-23 16:25:03 -0500

    avance

--- hola.js
+++ hola.js
@@ -1,8 +1,13 @@
 const formarGrupos = (N = 0, K = 0, T = []) => {
     const resultados_maximos = new Array(N + 1).fill(0).map(() => new Array(K + 1).fill(0));
+    let maximo = 0
+    let posicion = 0
 
     // Rellenamos el arreglo
     for (let i = 1; i <= N; i++) {
+        posicion = i
+        maximo = T[i - 1]
+
         for (let j = 1; j <= K; j++) {
             if (j === 0) {
                 resultados_maximos[i][j] = 0;
@@ -10,10 +15,68 @@ const formarGrupos = (N = 0, K = 0, T = []) => {
                 resultados_maximos[i][j] = T[i - 1] + resultados_maximos[i - 1][j]
             } else if (j > i) {
                 resultados_maximos[i][j] = resultados_maximos[i][j - 1]
-            } else {
-                console.log("Math.max(T.slice(0, 3)): ", Math.max(...T.slice(i - j - 1, i)))
-                console.log("T.slice(i - j - 1, i).length: ", T.slice(i - j - 1, i).length)
-                resultados_maximos[i][j] = Math.max(resultados_maximos[i - 1][j] + T[i - 1], Math.max(...T.slice(i - j - 1, i)) * T.slice(i - j - 1, i).length)
+            } else if (T[i]) {
+                /* console.log("Math.max(T.slice(0, 3)): ", Math.max(...T.slice(i - j - 1, i)))
+                console.log("T.slice(i - j - 1, i).length: ", T.slice(i - j - 1, i).length) */
+                // resultados_maximos[i][j] = Math.max(resultados_maximos[i - 1][j] + T[i - 1], Math.max(...T.slice(i - j - 1, i)) * T.slice(i - j - 1, i).length)
+                /*  resultados_maximos[i][j] = Math.max(
+                     resultados_maximos[i - 1][j],
+                     T[i - 1] + resultados_maximos[i - 1][j - T[i - 1]]) */
+
+                console.log("----------------------------------------")
+                /*  console.log(`${T[i - 1]} > ${T[i]}: `, T[i - 1] > T[i])
+                 console.log("posicion: ", posicion)
+                 console.log("maximo: ", maximo)
+                 console.log("T[i]: ", T[i])
+                 console.log("j - 1: ", j - 1) */
+                console.log("----------------------------------------")
+
+                /*  if (posicion <= j) {
+                     if (maximo > T[i]) {
+                         resultados_maximos[i][j] = maximo * posicion
+                     } else {
+ 
+                         resultados_maximos[i][j] = resultados_maximos[i - 1][j] + T[i - 1]
+                     }
+                 } else { */
+                posicion = 1
+                if (maximo > T[i]) {
+                    console.log("entre if 1 posicion: ", posicion)
+                    posicion++
+                    resultados_maximos[i][j] = resultados_maximos[i - posicion][j] + maximo * posicion
+                } else {
+                    if (posicion <= j) {
+
+                        maximo = T[i]
+                        posicion++
+
+                        resultados_maximos[i][j] = Math.max(resultados_maximos[i - posicion][j] + maximo * posicion, resultados_maximos[i - 1][j] + T[i - 1])
+
+                    } else {
+                        maximo = T[i]
+                        posicion = 1
+                        console.log("----------------------------------------")
+
+                        console.log("entre if 2 posicion: ", posicion)
+                        console.log("posicion: ", posicion)
+                        console.log("maximo: ", maximo)
+                        console.log("----------------------------------------")
+
+                        resultados_maximos[i][j] = Math.max(resultados_maximos[i - posicion][j] + maximo * posicion, resultados_maximos[i - 1][j] + T[i - 1])
+                    }
+                    /* maximo = T[i]
+                    console.log("entre if 2 posicion: ", posicion)
+                    posicion++
+                    console.log("entre if 2 maximo: ", maximo)
+
+                    // resultados_maximos[i][j] = resultados_maximos[i - 1][j] + T[i - 1]
+                    console.log("resultados_maximos[i - posicion][j]: ", resultados_maximos[i - posicion][j])
+                    console.log("i: ", i)
+                    resultados_maximos[i][j] = Math.max(resultados_maximos[i - posicion][j] + maximo * posicion, resultados_maximos[i - 1][j] + T[i - 1]) */
+                }
+
+
+
 
             }
         }
