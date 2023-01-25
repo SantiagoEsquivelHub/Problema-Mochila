eb09434c10dd81eaeafa109d441a7003c7392d97
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-01-24 23:35:58 -0500

    funcion para contruir la solución optima

--- hospital.js
+++ hospital.js
@@ -1,4 +1,5 @@
 const maximizarTapabojas = (N, T, P, C) => {
+    let auxiliar = new Array(N + 1).fill(0).map(() => new Array(P).fill(0));
     const tapabocas_maximos = new Array(N + 1).fill(0).map(() => new Array(P).fill(0));
 
     for (let n = 1; n <= N; n++) {
@@ -7,11 +8,42 @@ const maximizarTapabojas = (N, T, P, C) => {
                 tapabocas_maximos[n][p] = tapabocas_maximos[n - 1][p]
             } else {
                 tapabocas_maximos[n][p] = Math.max(tapabocas_maximos[n - 1][p], T[n - 1] + tapabocas_maximos[n - 1][p - C[n - 1]])
+
+                if (tapabocas_maximos[n - 1][p] > T[n - 1] + tapabocas_maximos[n - 1][p - C[n - 1]]) {
+                    tapabocas_maximos[n][p] = tapabocas_maximos[n - 1][p]
+                    auxiliar[n][p] = 0
+
+                } else {
+                    tapabocas_maximos[n][p] = T[n - 1] + tapabocas_maximos[n - 1][p - C[n - 1]]
+                    auxiliar[n][p] = 1
+                }
             }
         }
     }
 
+    let solucion = new Array(N - 1)
+    console.log("-------------------------")
+    solucion = mostrarResultados(auxiliar, N, P, C, solucion)
+    console.log("-------------------------")
+
     console.log("tapabocas_maximos: ", tapabocas_maximos)
+    console.log("solucion: ", solucion)
+}
+
+const mostrarResultados = (auxiliar, n, p, C, solucion = []) => {
+
+    if (n === 0) {
+        return solucion;
+    } else if (auxiliar[n][p] == 0) {
+        console.log("0")
+        solucion[n] = 0
+        mostrarResultados(auxiliar, n - 1, p, C, solucion)
+    } else {
+        console.log("1")
+        solucion[n] = 1
+        mostrarResultados(auxiliar, n - 1, p - C[n - 1], C, solucion)
+    }
+    return solucion
 }
 
 const P = 14
